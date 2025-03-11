53dd9e950b1f9753387db129f514ebecd92bd454
Ebad Momin <113835663+EBBYMOMIN@users.noreply.github.com>
2025-03-11 11:01:41 +0530

    Add files via upload

---
+++ Interview Queries.sql
@@ -0,0 +1,9 @@
+/*
+====================================================================================
+Project for a "Online Retail Company"
+This project will cover creating database, tables, and indexes, inserting data,
+and writing various queries for reporting and data analytics
+====================================================================================
+
+Project Overview: Online Retail Company
+------------------------------------------------------------------------------------
