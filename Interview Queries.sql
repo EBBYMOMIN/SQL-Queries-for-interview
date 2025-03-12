94e97b98ace6afa6ab3d7e0a02ab760f99f11a9d
Ebad Momin <113835663+EBBYMOMIN@users.noreply.github.com>
2025-03-12 18:01:14 +0530

    Add files via upload

--- Interview Queries.sql
+++ Interview Queries.sql
@@ -334,6 +334,10 @@ GROUP BY Country
 ORDER BY TotalCustomers DESC;
 
 19 -- Retreive the list of customers along with their total spending.
-
+SELECT c.CustomerID, c.FirstName, c.LastName, SUM(o.TotalAmount) AS TotalSpending
+FROM Customers c
+JOIN Orders o ON c.CustomerID = o.CustomerID
+GROUP BY c.CustomerID, c.FirstName, c.LastName
+ORDER BY TotalSpending DESC;
 
 20 -- List orders with more than a specified number of items (eg. 5 items)
